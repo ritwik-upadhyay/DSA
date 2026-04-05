59557772f3947103b78c30b8795e87cfc935039c
Ritwik Upadhyay <134372879+ritwik-upadhyay@users.noreply.github.com>
2026-04-05 19:51:17 +0530

    Added condns

--- Striver's A2Z DSA Sheet/Arrays/Hard/MaxProductSubarrayNegative.java
+++ Striver's A2Z DSA Sheet/Arrays/Hard/MaxProductSubarrayNegative.java
@@ -6,6 +6,8 @@ class MaxProductSubarrayNegative {
         int suff = 1;
         int ans = Integer.MIN_VALUE;
         for(int i=0;i<n;i++) {
+            if(pre==0)pre=1;
+            if(suff==0)suff=1;
             pre *= a[i];
             suff *= a[n-i-1];
             ans = Math.max(ans,Math.max(pre,suff));
